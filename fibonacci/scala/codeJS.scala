1caee0a6b8b8259eb42106d64fb27ff3877ba672
虎鸣 <hepin.p@alibaba-inc.com>
2024-12-22 15:18:51 +0800

    chore: add scala-js support for fibonacci

---
+++ fibonacci/scala/codeJS.scala
@@ -0,0 +1,18 @@
+//> using scala 3.6.2
+//> using file base.scala
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
+
+@js.native
+@JSGlobal("process")
+object NodeProcess extends js.Object {
+  val argv: js.Array[String] = js.native
+}
+
+object MainJS {
+  def main(args: Array[String]): Unit = {
+    val params = NodeProcess.argv.drop(2).toArray
+    Fibonacci.run(params(0))
+  }
+}
