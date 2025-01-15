5bf8c5036f61ba8c4ce3be01eba5b5428938440e
PEZ <pez@pezius.com>
2025-01-15 23:48:20 +0100

    in-process-benching: Add Clojure and Clojure Native loops

---
+++ loops/clojure/run.clj
@@ -0,0 +1,29 @@
+(ns run
+  (:require [languages.benchmark :as benchmark])
+  (:gen-class))
+
+(set! *unchecked-math* :warn-on-boxed)
+
+(defn- loops [^long u]
+  (let [r (long (rand-int 10000)) ; Get a random number 0 <= r < 10k
+        a (long-array 10000)] ; Array of 10k elements initialized to 0
+    (loop [i 0]
+      (when (< i 10000) ; 10k outer loop iterations
+        (loop [j 0]
+          (when (< j 10000) ; 10k inner loop iterations, per outer loop iteration
+            (aset a i (unchecked-add (aget a i) (rem j u))) ; Simple sum
+            (recur (unchecked-inc j))))
+        (aset a i (unchecked-add (aget a i) r)) ; Add a random value to each element in array
+        (recur (unchecked-inc i))))
+    (aget a r)))
+
+(defn -main [& args]
+  (let [run-ms (parse-long (first args))
+        u (parse-long (second args))
+        _ (benchmark/run run-ms #(loops u))
+        [result count mean-time] (benchmark/run run-ms #(loops u))]
+    (println (str (double mean-time) ";" count ";" result))))
+
+(comment
+  (-main "1" "40")
+  :rcf)
