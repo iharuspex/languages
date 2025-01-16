5325d6d6f1dd9023f1b90f5f45b0ceaf950dec1d
Samuel S. Shepard <sammysheep@gmail.com>
2025-01-15 20:56:34 -0500

    C-like min thanks to @PizzasBear. Promote safety checks to main.

--- levenshtein/rust/code.rs
+++ levenshtein/rust/code.rs
@@ -40,30 +40,34 @@ fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
 
     if args.len() < 2 {
-        println!("Please provide at least two strings as arguments.");
+        eprintln!("Please provide at least two strings as arguments.");
         return;
     }
 
-    let mut min_distance = None;
+    if !args.iter().all(|s| s.is_ascii()) {
+        eprintln!("This benchmark assumes ASCII string values.");
+        return;
+    }
+
+    if args.iter().any(|s| s.is_empty()) {
+        eprintln!("This benchmark assumes non-empty strings.");
+        return;
+    }
+
+    let mut min_distance = u32::MAX;
     let mut times = 0;
 
     // Compare all pairs of strings
-    for i in 0..args.len() {
-        for j in 0..args.len() {
+    for (i, s1) in args.iter().enumerate() {
+        for (j, s2) in args.iter().enumerate() {
             if i != j {
-                let distance = levenshtein_distance(args[i].as_bytes(), args[j].as_bytes());
-                if let Some(current_min) = min_distance {
-                    if distance < current_min {
-                        min_distance = Some(distance);
-                    }
-                } else {
-                    min_distance = Some(distance);
-                }
+                let distance = levenshtein_distance(s1.as_bytes(), s2.as_bytes());
+                min_distance = min_distance.min(distance);
                 times += 1;
             }
         }
     }
 
-    println!("times: {}", times);
-    println!("min_distance: {}", min_distance.unwrap_or(u32::MAX));
+    println!("times: {times}");
+    println!("min_distance: {min_distance}");
 }
