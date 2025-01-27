494c29e614e3b87fd540ab79e2e3d074e0b562ed
PEZ <pez@pezius.com>
2025-01-27 23:37:06 +0100

    Babashka: Extract fibonacci.clj

--- fibonacci/bb/run.clj
+++ fibonacci/bb/run.clj
@@ -1,14 +1,9 @@
 (require '[languages.benchmark :as benchmark])
-
-(defn- fibonacci [n]
-  (if (< n 2)
-    n
-    (+ (fibonacci (- n 1))
-       (fibonacci (- n 2)))))
+(require 'fibonacci)
 
 (let [run-ms (parse-long (first *command-line-args*))
       ; skip warmup arg, because we skip warmups
       u (parse-long (nth *command-line-args* 2))]
-  (-> (benchmark/run #(fibonacci u) run-ms)
+  (-> (benchmark/run #(fibonacci/fibonacci u) run-ms)
       benchmark/format-results
       println))
