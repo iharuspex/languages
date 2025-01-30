6332a16b9c6e65dfaec11872f028cb4d989892a8
Tamnac <iberoo233@gmail.com>
2025-01-30 15:55:50 -0800

    fix run.cr

--- fibonacci/crystal/run.cr
+++ fibonacci/crystal/run.cr
@@ -5,6 +5,6 @@ run_t = ARGV[0].to_i
 warmup_t = ARGV[1].to_i
 input = ARGV[2].to_i
 
-bench(input, warmup_t) {|n| fibonacci(n)}
+bench(warmup_t) {fibonacci(input)}
 
-bench(input, run_t) {|n| fibonacci(n)}
+bench(run_t) {fibonacci(input)}
