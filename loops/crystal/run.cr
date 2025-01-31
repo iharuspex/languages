e977af10f9f177a669d28e21d91f3e7617c3bc92
Tamnac <iberoo233@gmail.com>
2025-01-30 18:15:12 -0800

    port loops

---
+++ loops/crystal/run.cr
@@ -0,0 +1,10 @@
+require "./loops.cr"
+require "../../lib/crystal/benchmark.cr"
+
+run_t = ARGV[0].to_i
+warmup_t = ARGV[1].to_i
+input = ARGV[2].to_i
+
+bench(warmup_t) {loops(input)}
+
+bench(run_t) {loops(input)}
