cbc848adf531d4ce4d0e19f3fe4adde9d29e96cf
Tamnac <iberoo233@gmail.com>
2025-02-01 14:14:05 -0800

    crystal: port loops and fibonacci to new benchmark

--- fibonacci/crystal/run.cr
+++ fibonacci/crystal/run.cr
@@ -5,6 +5,7 @@ run_t = ARGV[0].to_i
 warmup_t = ARGV[1].to_i
 input = ARGV[2].to_i
 
-bench(warmup_t, warmup?: true) {fibonacci(input)}
+bench(warmup_t) { fibonacci(input) }
 
-bench(run_t) {fibonacci(input)}
+result = bench(run_t) { fibonacci(input) }
+puts format_bench(result, &.itself)
