1c5ae6ff0340010b0ed60842565b3016e0045991
Tamnac <49466795+Tamnac@users.noreply.github.com>
2025-02-01 13:12:59 -0800

    fix comment
    Co-authored-by: Peter Strömberg <pez@pezius.com>

--- loops/crystal/loops.cr
+++ loops/crystal/loops.cr
@@ -3,7 +3,7 @@ def loops(u : Int32) : Int32
 	a = StaticArray(Int32, 10_000).new(0)         # Array of 10k elements initialized to 0
 
 	10_000.times do |i|                       # 10k outer loop iterations
-		10_000.times do |j|                    # 100k inner loop iterations, per outer loop iteration
+		10_000.times do |j|                     # 10k inner loop iterations, per outer loop iteration
 			a[i] += j % u                             # Simple sum
 		end
 		a[i] += r                                   # Add a random value to each element in array
