ba19617fe4a0cb5e13f09b282fbc771e27abd438
Tamnac <iberoo233@gmail.com>
2025-01-31 22:08:43 -0800

    add warmup indication of

--- fibonacci/crystal/run.cr
+++ fibonacci/crystal/run.cr
@@ -5,6 +5,6 @@ run_t = ARGV[0].to_i
 warmup_t = ARGV[1].to_i
 input = ARGV[2].to_i
 
-bench(warmup_t) {fibonacci(input)}
+bench(warmup_t, warmup?: true) {fibonacci(input)}
 
 bench(run_t) {fibonacci(input)}
--- loops/crystal/run.cr
+++ loops/crystal/run.cr
@@ -5,6 +5,6 @@ run_t = ARGV[0].to_i
 warmup_t = ARGV[1].to_i
 input = ARGV[2].to_i
 
-bench(warmup_t) {loops(input)}
+bench(warmup_t, warmup?: true) {loops(input)}
 
 bench(run_t) {loops(input)}
