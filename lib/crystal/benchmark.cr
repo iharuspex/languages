4a750a4e8e9d9961013f436a9283a0141f19f3a5
PEZ <pez@pezius.com>
2025-02-03 10:17:23 +0100

    In-process runner: Crystal: Apply feedback from @Tamnac

--- lib/crystal/benchmark.cr
+++ lib/crystal/benchmark.cr
@@ -1,7 +1,7 @@
 module Enumerable
     def variance
       mean = self.sum / self.size
-      sum = self.reduce(0.0) { |acc, cur| acc + (cur-mean) ** 2 }
+      sum = self.reduce(0) { |acc, cur| acc + (cur-mean) ** 2 }
       sum/(self.size - 1).to_f
     end
 
@@ -11,14 +11,14 @@ module Enumerable
 end
 
 def bench(run_ms : Int32, &fn)
-	times = Array(Float64).new
+	times = Array(Int64).new
 	result = 0
 	secs = 0
 	run_ns = run_ms * 1_000_000.0
 
 	while(times.sum < run_ns && !(times.sum == 0 && times.size > 0))
 		a = Time.measure {result = yield}
-		times << a.nanoseconds.to_f
+		times << a.nanoseconds
 
 		if (times.sum / 1_000_000_000).round > secs
 			STDERR.print '.'
@@ -34,7 +34,7 @@ def format_bench(data, &formatter)
 	raise "no data!" if data[:times].empty?
 
 	result = yield data[:result]
-	times = data[:times].map { |t| t / 1_000_000 } # Convert nanoseconds to milliseconds
+	times = data[:times].map &./(1_000_000)
 
 	# mean_ms,std-dev-ms,min_ms,max_ms,times,result
 	"#{times.sum/times.size},#{times.std_dev},#{times.min},#{times.max},#{times.size},#{result}"
