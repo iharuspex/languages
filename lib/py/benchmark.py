830b9713165fe600939f6c95df8258e6df811d0e
PEZ <pez@pezius.com>
2025-02-06 21:49:56 +0100

    Python in-process: Add benchmark utility

---
+++ lib/py/benchmark.py
@@ -0,0 +1,44 @@
+import time
+import sys
+
+def variance(array):
+    mean = sum(array) / len(array)
+    return sum((x - mean) ** 2 for x in array)
+
+def std_dev(array):
+    return variance(array) ** 0.5
+
+def bench(run_ms, func):
+    times = []
+    result = None
+    run_ns = run_ms * 1_000_000
+
+    start_time = time.monotonic_ns()
+    while sum(times) < run_ns:
+        start = time.monotonic_ns()
+        result = func()
+        end = time.monotonic_ns()
+        elapsed = end - start
+        times.append(elapsed)
+
+        if run_ms > 1 and (sum(times) // 1_000_000_000) > (sum(times[:-1]) // 1_000_000_000):
+            sys.stderr.write('.')
+            sys.stderr.flush()
+
+    if run_ms > 1:
+        sys.stderr.write('\n')
+
+    return {
+        'times': times,
+        'result': result
+    }
+
+def format_bench(data):
+    if not data['times']:
+        raise ValueError("no data!")
+
+    result = data['result']
+    times = [t / 1_000_000 for t in data['times']]  # convert to milliseconds
+
+    # mean_ms,std-dev-ms,min_ms,max_ms,times,result
+    return f"{sum(times) / len(times)},{std_dev(times)},{min(times)},{max(times)},{len(times)},{result}"
