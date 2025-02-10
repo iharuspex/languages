948299ff347075a6e384445fb61b5fa2586a2ac1
PEZ <pez@pezius.com>
2025-02-10 23:52:56 +0100

    Julia in-process: Use array index assignment like all other langs

--- loops/julia/loops.jl
+++ loops/julia/loops.jl
@@ -3,16 +3,15 @@ module Loops
 export loops
 
 function loops(u::Int)::Int
-    a = zeros(Int, 10^4)
-    r = rand(1:10^4)
-    @inbounds for i in 1:10^4
-        local sum = 0
-        @inbounds @simd for j in 1:10^4
-            sum += j % u
+    a = zeros(Int, 10^4)          # Allocate an array of 10,000 zeros
+    r = rand(1:10^4)              # Choose a random index between 1 and 10,000
+    @inbounds for i in 1:10^4     # Outer loop over array indices
+        @inbounds for j in 1:10^4 # Inner loop: 10,000 iterations per outer loop iteration
+            a[i] += j % u         # Simple sum
         end
-        a[i] += sum + r
+        a[i] += r                 # Add a random value to each element in array
     end
-    return a[r]
+    return a[r]                   # Return the element at the random index
 end
 
-end # module Loops
+end  # module Loops
