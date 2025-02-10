ea1fb19238fb84e59d61457c8136f47819467dca
D. Ben Knoble <ben.knoble+github@gmail.com>
2025-02-09 21:34:00 -0500

    racket(lib): use idiomatic structure
    Prefer cond and define over let except for simple expressions. See
    "Choosing the Right Construct" [1].
    
    [1]: https://docs.racket-lang.org/style/Choosing_the_Right_Construct.html

--- lib/racket/benchmark.rkt
+++ lib/racket/benchmark.rkt
@@ -18,63 +18,68 @@
 ;; Returns a hash with keys:
 ;;   'runs, 'result, 'mean-ms, 'min-ms, 'max-ms, 'std-dev-ms
 (define (run f run-ms)
-  (if (= run-ms 0)
-      (hash 'runs 0
-            'result (f)
-            'mean-ms 0
-            'min-ms 0
-            'max-ms 0
-            'std-dev-ms 0)
-      (let* ([run-ns (* run-ms 1000000)] ; convert run-ms (milliseconds) to nanoseconds
-             [init-t (current-monotonic-nanotime)]
-             [last-status-t init-t])
-        (when (> run-ms 1)
-          (display "." (current-error-port))
-          (flush-output (current-error-port)))
-        (define (loop last-tet results last-status-t)
-          (let* ([t0 (current-monotonic-nanotime)]
-                 [result (f)]
-                 [t1 (current-monotonic-nanotime)]
-                 [elapsed-time (- t1 t0)]
-                 [total-elapsed-time (+ last-tet elapsed-time)]
-                 [timed-result (list total-elapsed-time elapsed-time result)]
-                 [print-status? (and (> run-ms 1)
-                                     (> (- t0 last-status-t) 1000000000))])
-            (when print-status?
-              (display "." (current-error-port))
-              (flush-output (current-error-port)))
-            (if (< total-elapsed-time run-ns)
-                (loop total-elapsed-time
-                      (cons timed-result results)
-                      (if print-status? t1 last-status-t))
-                (reverse (cons timed-result results)))))
-        (let* ([final-results (loop 0 '() last-status-t)]
-               ;; Get the final timed result (i.e. from the last run)
-               [last-run (car (reverse final-results))]
-               [total-elapsed-time (first last-run)]
-               [result (list-ref last-run 2)]
-               [elapsed-times (map second final-results)]
-               [runs (length final-results)]
-               [mean-ns (/ total-elapsed-time runs)]
-               [min-ns (apply min elapsed-times)]
-               [max-ns (apply max elapsed-times)]
-               [variance (/ (apply + (map (λ (t)
-                                            (expt (- t mean-ns) 2))
-                                          elapsed-times))
-                            runs)]
-               [std-dev-ns (sqrt variance)]
-               [mean-ms (/ mean-ns 1000000.0)]
-               [min-ms (/ min-ns 1000000.0)]
-               [max-ms (/ max-ns 1000000.0)]
-               [std-dev-ms (/ std-dev-ns 1000000.0)])
-          (when (> run-ms 1)
-            (newline (current-error-port)))
-          (hash 'runs runs
-                'result result
-                'mean-ms mean-ms
-                'min-ms min-ms
-                'max-ms max-ms
-                'std-dev-ms std-dev-ms)))))
+  (cond
+    [(zero? run-ms)
+     (hash 'runs 0
+           'result (f)
+           'mean-ms 0
+           'min-ms 0
+           'max-ms 0
+           'std-dev-ms 0)]
+    [else
+     ; convert run-ms (milliseconds) to nanoseconds
+     (define run-ns (* run-ms 1000000))
+     (define init-t (current-monotonic-nanotime))
+     (define last-status-t init-t)
+     (when (> run-ms 1)
+       (display "." (current-error-port))
+       (flush-output (current-error-port)))
+     (define final-results
+       (let loop ([last-tet 0]
+                  [results '()]
+                  [last-status-t last-status-t])
+         (define t0 (current-monotonic-nanotime))
+         (define result (f))
+         (define t1 (current-monotonic-nanotime))
+         (define elapsed-time (- t1 t0))
+         (define total-elapsed-time (+ last-tet elapsed-time))
+         (define timed-result (list total-elapsed-time elapsed-time result))
+         (define print-status? (and (> run-ms 1)
+                                    (> (- t0 last-status-t) 1000000000)))
+         (when print-status?
+           (display "." (current-error-port))
+           (flush-output (current-error-port)))
+         (if (< total-elapsed-time run-ns)
+             (loop total-elapsed-time
+                   (cons timed-result results)
+                   (if print-status? t1 last-status-t))
+             (reverse (cons timed-result results)))))
+     ;; Get the final timed result (i.e. from the last run)
+     (define last-run (last final-results))
+     (define total-elapsed-time (first last-run))
+     (define result (third last-run))
+     (define elapsed-times (map second final-results))
+     (define runs (length final-results))
+     (define mean-ns (/ total-elapsed-time runs))
+     (define min-ns (apply min elapsed-times))
+     (define max-ns (apply max elapsed-times))
+     (define variance (/ (apply + (map (λ (t)
+                                         (expt (- t mean-ns) 2))
+                                       elapsed-times))
+                         runs))
+     (define std-dev-ns (sqrt variance))
+     (define mean-ms (/ mean-ns 1000000.0))
+     (define min-ms (/ min-ns 1000000.0))
+     (define max-ms (/ max-ns 1000000.0))
+     (define std-dev-ms (/ std-dev-ns 1000000.0))
+     (when (> run-ms 1)
+       (newline (current-error-port)))
+     (hash 'runs runs
+           'result result
+           'mean-ms mean-ms
+           'min-ms min-ms
+           'max-ms max-ms
+           'std-dev-ms std-dev-ms)]))
 
 ;; format-results : hash -> string
 ;; Formats the benchmark results as:
